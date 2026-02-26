a00e383fdc02193111f6169f22f252b3196e8e7f
Rui Franscisco de Paula Inácio Diniz <rui.pdiniz@gmail.com>
2026-02-26 10:42:10 -0300

    Próximo passo: relatório mensal operacional com período

--- tests/Feature/OpenApiContractTest.php
+++ tests/Feature/OpenApiContractTest.php
@@ -30,10 +30,14 @@ $required = [
     '/families/{id}' => ['get', 'put', 'delete'],
     '/street/people' => ['get', 'post'],
     '/deliveries/events' => ['get', 'post'],
+    '/deliveries/events/{id}/publish' => ['post'],
     '/equipment' => ['get', 'post'],
     '/reports/summary' => ['get'],
+    '/reports/monthly' => ['get'],
     '/settings/eligibility' => ['get', 'put'],
     '/eligibility/check' => ['post'],
+    '/visits' => ['get', 'post'],
+    '/visits/{id}/complete' => ['post'],
 ];
 
 foreach ($required as $path => $methods) {
